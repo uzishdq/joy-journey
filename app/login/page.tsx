b697e53651027f050c84d32be1c35516685954cf
uzishdq <uzi.affinity@gmail.com>
2025-12-29 23:53:34 +0700

    fix: bg login, register

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import LoginForm from "@/components/form/login-form";
 
 export default function LoginPage() {
   return (
-    <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+    <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-linear-to-t from-amber-600 via-yellow-400 to-orange-300">
       <div className="w-full max-w-lg">
         <LoginForm />
       </div>
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "@/components/form/register-form";
 
 export default function RegisterPage() {
   return (
-    <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-linear-to-t from-indigo-500 to-blue-500">
+    <main className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-linear-to-t from-amber-600 via-yellow-400 to-orange-300">
       <div className="w-full max-w-lg">
         <RegisterForm />
       </div>
